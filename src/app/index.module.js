ee9469235f6c42a38f782854ac48f97f68fed69c
Marcela Gotta <marcelagotta@gmail.com>
2015-10-26 15:50:30 +0100

    Primera Subida

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mg', ['ngTouch', 'ngMessages', 'ngAria', 'ngRoute', 'toastr']);
+
+})();
---
+++ src/app/index.route.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mg')
+    .config(routeConfig);
+
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+
+})();
