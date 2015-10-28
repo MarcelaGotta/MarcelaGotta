5ccbe7b03fb95f7dd7ebdc69e092b4bfd2594808
Marcela Gotta <marcelagotta@gmail.com>
2015-10-28 17:56:29 +0100

    Next Version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 $( document ).ready(function() {
-    $('.dialog-welcome')
-        .typist({
-            text: 'Sed ut perspiciatis, unde omnis ой, что-то пошло не так',
-            speed: '20'
-        })
-        .typistPause(1000) // 2 sec
-        .typistRemove(23)
-        .typistAdd('iste natus error sit voluptatem accusantium doloremque laudantium.')
-        .typistStop();
-    
+    $("#typed").typed({
+            stringsElement: $('#typed-strings')
+        });
 });
